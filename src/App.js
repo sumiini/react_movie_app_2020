87dc8abc60e37794638314f6e4cd961609c6b05c
sumin_hong <sumiiniii@ajou.ac.kr>
2020-08-14 16:17:01 +0900

    jsx_props_study

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
 
+// App component 에서 Food component로 정보를 보내고 그다음 food component에서 그 정보를 어떻게 사용하는가
+function Food({name,picture,rating}){ //props.fav 랑 {fav}랑 같은거
+  //console.log(props.fav);
+  return (
+    <div>
+      <h2>I like {name} </h2>
+      <h4> {rating}/5.0 </h4>
+      <img src={picture} alt={name} /> 
+    </div>
+    
+  );
+}
+
+Food.propTypes={
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+
+};
+
+// food의 object 배열
+const foodILike=[{id:1,name: "kimchi" ,image: "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi",rating:5},
+{id:2,name: "ramen", image: "https://www.crowdpic.net/photos/%EB%9D%BC%EB%A9%B4%EC%82%AC%EC%A7%84",rating:4.3}];
+
+// <Food fav="kimchi"/> --> 이거는 Food component에 fav라는 이름의 property를 kimchi라는 value로 준 것
+// component는 HTML을 반환하는 함수이다.
+// foodILike.map(dishi => <Food name={dish.name} />)} 에서 각 dish는 object이다.
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>
+
+      
+      {foodILike.map(dish => <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>)} 
+
+
     </div>
   );
 }
 
 export default App;
+
+// map은 array의 각 item에서 function을 실행하는 array를 가지는 javascript function이며 ,
+// 그 function의 result를 갖는 array를 반환
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-
+// <App /> 이 부분이 component를 사용하고자 할 때, component의 형태이다. react는 component를 사용해서 html처럼 작성해야한다.
+// 그래서 javascript와 html 사이의 이러한 조합을 jsx라고 부른다.
+// react application은 한 번에 하나의 component만 rendering 할 수 있다.
+// 그래서 모든 것은 App안에 들어가야한다. App안에서 많은 component를 넣을 수 있다.
+// 그리고 이러한 component안에 더 많은 component를 import 할 수 있다.
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();

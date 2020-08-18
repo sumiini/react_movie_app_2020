7919db61436bfa4f0bbf432dec9da900727e64a1
sumin_hong <sumiiniii@ajou.ac.kr>
2020-08-18 15:32:20 +0900

    n_final

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from "axios";
-import Movie from "./Movie";
+import Movie from "./components/Movie";
+import "./App.css";
 class App extends React.Component{
 
   state={
@@ -11,20 +12,26 @@ class App extends React.Component{
   //axios.get 은 완료되기까지 시간이 조금 필요하기때문에 await를 넣음
   getMovies=async()=>{
     const {data:{data:{movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-    this.setState({movies:movies,isLoading:false})
+    this.setState({movies,isLoading:false})
   }
   componentDidMount(){
     this.getMovies();
   }
   render(){
    const {isLoading, movies}=this.state;
-   return <div> {isLoading ? "Loading...":movies.map(movie =>(
-   <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image}
-    />
-   ))}</div>
+   return <section className="container"> 
+     {isLoading ? <div className="loader">
+       <span className="loader__text">Loading...</span>
+
+     </div>:
+     <div className="movies">
+       {movies.map(movie => (
+         <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} genres={movie.genres}/>))}
+     </div>
+
+     }</section>
   }
 }
-
 export default App;
 
 // map은 array의 각 item에서 function을 실행하는 array를 가지는 javascript function이며 ,
